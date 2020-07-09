11730ca00339af988ac4bf586166a15dbcc6e19f
Glitch (oval-jeweled-raptorex) <none>
2020-07-09 19:55:03 +0000

    🚇🏎 Checkpoint ./script.js:18530910/8168

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
 // Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
 // Add to this list as you consult the p5.js documentation for other functions.
 /* global angleMode, DEGREES, arc, clear, createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
-          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, tint, noTint
+          rect, rectMode, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, tint, noTint
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, noFill, windowWidth, windowHeight, noStroke, 
-          keyCode, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
+          keyCode, CENTER, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
-// We'll use variables for most of our colors in this code-along.
-let backgroundColor;
-let color1;
-let color2;
-let textColor;
+let brushHue;
+let priorX;
+let priorY;
 
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
-  noStroke();
-
-  // When used with only one argument, the color mode is greyscale.
-  // 0 is black and 100 is white.
-  backgroundColor = color(95);
-  textColor = color(20);
-  // When used with three arguments, the function takes, in this order:
-  // HUE - 0 to 360 degrees on a color wheel - 0 is red, 120 is green and 240
-  //       is blue.
-  // SATURATION - 0 is no color (greyscale), and 100 is as bold as possible.
-  // BRIGHTNESS - 0 is no light (black), and 100 is as bright as possible.
-  color1 = color(0, 80, 80);
-  color2 = color(200, 80, 80);
+  brushHue = 0;
+  priorX = 0;
+  priorY = 0;
+  background(95);
+  strokeWeight(6);
 }
 
 function draw() {
-  background(backgroundColor);
-  // Call the drawCenterLine function here to run the three lines of code
-  // contained in that function.
+  chooseColors();
 
-  // The red and blue circles:
-  fill(color1);
-  ellipse(100, 200, 50);
-  fill(color2);
-  ellipse(300, 200, 50);
+  if (mouseIsPressed) {
+    // Pick one of the three behaviors below:
+    // rect(mouseX, mouseY, 15, 15);
+    // ellipse(random(width), random(height), 30, 30);
+    // line(priorX, priorY, mouseX, mouseY);
+    line(width-priorX, height-priorY, width-mouseX, height-mouseY);
+  }
 
-  // The grey circle and the text:
-  fill(textColor);
-  ellipse(50, 50, 50);
-  text("Flip the switch", 20, 20);
+  // Store the mouseX and mouseY from this frame in order to use them next
+  // frame - remember from the DVD lesson that the draw loop runs once every
+  // frame.
+  priorX = mouseX;
+  priorY = mouseY;
 }
 
-function drawCenterLine() {
-  // This function will turn stroke on, draw the line, and then turn stroke
-  // back off.
-  // A line segment in p5.js has four arguments: x1, y1, x2, y2
-  stroke(textColor);
-  line(200, 0, 200, 400);
-  noStroke();
+function chooseColors() {
+  brushHue += 1;
+  brushHue %= 360;
+  stroke(brushHue, 50, 80);
+  fill(brushHue, 50, 80);
 }
+
+function keyPressed() {
+  background(95);
+}
+
+// function mousePressed() {
+//   ellipse(random(width), random(height), 30, 30);
+// }
