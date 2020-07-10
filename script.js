83d8465109223783c8e51683d648e272bef2ec94
Glitch (oval-jeweled-raptorex) <none>
2020-07-10 22:24:32 +0000

    ⛩🌨 Checkpoint ./script.js:18096514/1694:18479949/51

--- script.js
+++ script.js
@@ -12,7 +12,14 @@ let strokeSize;
 let strokeDelta;
 let drawingGraphics;
 
-// k means clustering on george floyd image then, 
+
+// uses mouse to draw on an image
+// sliders change how the image is distorted
+// mouseX, mouseY would pick up the color and maybe change it
+
+//brush size/type
+//color
+//
 
 
 let georgeFloydImg
@@ -20,63 +27,10 @@ let georgeFloydImg
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
-  background(0);
-  brushHue = 0;   
-  priorX = 0;
-  priorY = 0;
-  strokeSize = 6;
-  strokeDelta = 0.2;
-  drawingGraphics = createGraphics(width, height);
 
-  colorMode(HSB, 360, 100, 100);
-  drawingGraphics.colorMode(HSB, 360, 100, 100);
 }
 
 function draw() {
   background(95);
-  chooseColors();
-
-  if (mouseIsPressed) {
-    // Pick one of the three behaviors below:
-    // rect(mouseX, mouseY, 15, 15);
-    // ellipse(random(width), random(height), 30, 30);
-    drawingGraphics.line(priorX, priorY, mouseX, mouseY);
-    // line(width-priorX, height-priorY, width-mouseX, height-mouseY);  // Mirrored line
-  }
-  image(drawingGraphics, 0, 0, width, height);
-
-  // Draw a preview of the brush on top.
-  ellipse(mouseX, mouseY, strokeSize);
-
-  // Store the mouseX and mouseY from this frame in order to use them next
-  // frame - remember from the DVD lesson that the draw loop runs once every
-  // frame.
-  priorX = mouseX;
-  priorY = mouseY;
+  
 }
-
-function chooseColors() {
-  brushHue += 1;
-  brushHue %= 360;
-  if (strokeSize > 30 || strokeSize <= 1) {
-    strokeDelta = -strokeDelta;
-  }
-  strokeSize += strokeDelta;
-
-  drawingGraphics.strokeWeight(strokeSize);
-  drawingGraphics.stroke(brushHue, 50, 80);
-  drawingGraphics.fill(brushHue, 50, 80);
-
-  // Set the fill for the brush preview as well
-  fill(brushHue, 50, 80);
-}
-
-function keyPressed() {
-  drawingGraphics.background(95);
-  console.log("key: " + key);
-  console.log("keyCode: " + keyCode);
-}
-
-// function mousePressed() {
-//   drawingGraphics. ellipse(random(width), random(height), 30, 30);
-// }
