203711b9a493c845d51a65b86bfad81a04c37747
Glitch (oval-jeweled-raptorex) <none>
2020-07-10 23:58:44 +0000

    🌴🏛 Checkpoint ./script.js:18096514/835:18479949/247

--- script.js
+++ script.js
@@ -3,26 +3,21 @@
 /* global loadPixels, get, red, green, blue, angleMode, createGraphics, DEGREES, arc, clear, createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
           rect, random, keyIsPressed, rectMode, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, tint, noTint
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, noFill, windowWidth, windowHeight, noStroke, 
-          key, keyCode, createSlider, resize,CENTER, KeyR, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
-let imageGraphics;
+          key, keyCode, createInput, createSlider, resize,CENTER, KeyR, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
+const drawingGraphicsHeight = 300;
+const drawingGraphicsWidth = 500;
 
-let brushHue;
-let priorX;
-let priorY;
 let strokeSize;
 let strokeDelta;
 //let drawingGraphics;
 let squareSize=10;
 let sizeSlider;
 
+//user image input button
+let input;
+let button;
+let greeting;
 
-
-// uses mouse to draw on an image
-// sliders change how the image is distorted
-// mouseX, mouseY would pick up the color and maybe change it
-
-//brush size, square
-//color
 
 let brushColor;
 
@@ -35,22 +30,28 @@ let drawingGraphics;
 
 function setup() {
   // Canvas & color settings
-  createCanvas(500, 600);
+  createCanvas(drawingGraphicsWidth, 2*drawingGraphicsHeight);
+  background(0);
+  
+  input = createInput();
+ 
 
-  
-  
-  
   loadImage("https://www.pewtrusts.org/-/media/post-launch-images/2016/12/chicago_skyline/chicago_skyline_16x9.jpg", bgImage => {
-    bgImage.resize(0, 600);
+    bgImage.resize(0, drawingGraphicsHeight);
     image(bgImage, 0, 0);
   });
+  loadImage("https://www.pewtrusts.org/-/media/post-launch-images/2016/12/chicago_skyline/chicago_skyline_16x9.jpg", bgImage => {
+    bgImage.resize(0, drawingGraphicsHeight);
+    image(bgImage, 0, drawingGraphicsHeight);
+  });
+  
   loadPixels();
   
   sizeSlider = createSlider(1,20);
   sizeSlider.position(20,20);
   
   //setting up drawing layer
-  drawingGraphics = createGraphics(width, height);
+  drawingGraphics = createGraphics(drawingGraphicsWidth, drawingGraphicsHeight);
   drawingGraphics.noStroke();
   
 }
@@ -68,7 +69,7 @@ function drawFromMouse(){
   if(mouseIsPressed){
     drawingGraphics.fill(getMouseColor());  //getMouseColor()
     drawingGraphics.rect(mouseX,mouseY,strokeSize,strokeSize);
-    image(drawingGraphics, 0, 0, width, height);
+    image(drawingGraphics, 0, 0, drawingGraphicsWidth, drawingGraphicsHeight);
   
     //uses mouse location
   }
@@ -100,7 +101,7 @@ function setBrush(){
 }
 
 function getMouseColor(){  //returns color of canvas under mouse 
-  let canvasPixel = get(mouseX, mouseY);
+  let canvasPixel = get(mouseX, mouseY+drawingGraphicsHeight);
   
   return canvasPixel;
 }
