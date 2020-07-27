004c89e004d6dddcc93276e6dd972977e2a809e7
Glitch (oval-jeweled-raptorex) <none>
2020-07-27 22:47:11 +0000

    🌘🍐 Checkpoint ./index.html:18047512/1616:18479949/9551 ./script.js:18479949/6765 ./JavaScript:18047512/6765 ./style.css:18047512/244

--- mapsapi.js
+++ mapsapi.js
@@ -1,9 +1,3 @@
-// Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
-// Add to this list as you consult the p5.js documentation for other functions.
-/* global loadPixels, get, red, green, blue, angleMode, createGraphics, DEGREES, arc, clear, createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
-          rect, random, keyIsPressed, rectMode, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, tint, noTint
-          mouseX, mouseY, strokeWeight, line, mouseIsPressed, noFill, windowWidth, windowHeight, noStroke, 
-          key, keyCode, textAlign, createInput, createButton, createElement, createSlider, resize,CENTER, KeyR, PI, HALF_PI, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 /**
  * Firebase config block.
  */
