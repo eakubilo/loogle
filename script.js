7c7c44feb2a821d4d6204657ac757f8da03b71df
Glitch (project-week-yelp-for-toilets) <none>
2020-07-28 11:53:52 +0000

    🚤🐉 Checkpoint ./index.html:18104489/433 ./review/index.html:18104489/37 ./script.js:18104489/2179

--- script.js
+++ script.js
@@ -20,40 +20,46 @@ function addMarker(props, map) {
   }
 }
 
-
-class Bathroom{
-  
-  constructor(lati, long,rating,tp,address,sink,soap,handSani,smellG,bathRoomName){
+class Bathroom {
+  constructor(
+    lati,
+    long,
+    rating,
+    tp,
+    address,
+    sink,
+    soap,
+    handSani,
+    smellG,
+    bathRoomName
+  ) {
     this.lati = lati;
     this.long = long;
     this.rating = 0;
     this.address = address;
     this.sink = sink;
-    this.soap= soap;
+    this.soap = soap;
     this.handSani = handSani;
-    this.smellG= smellG;
+    this.smellG = smellG;
     this.tp = tp;
     this.bathRoomName = bathRoomName;
   }
-  
-  score(){
-    if(this.sink){
-     this. rating++;
-    }
-    if(this.soap){
-      this.rating ++;
-    }
-    if(this.handSani){
-      this.rating += .5;
-    }
-    if(this.tp){
-      this.rating ++;
-    }
-    if(this.smellG){
-      this.rating ++
-    }
-    
-    
-  }
-  }
 
+  score() {
+    if (this.sink) {
+      this.rating++;
+    }
+    if (this.soap) {
+      this.rating++;
+    }
+    if (this.handSani) {
+      this.rating += 0.5;
+    }
+    if (this.tp) {
+      this.rating++;
+    }
+    if (this.smellG) {
+      this.rating++;
+    }
+  }
+}
